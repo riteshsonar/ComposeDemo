7a605decab0c45a7b5155df455471c8f516e0bfe
Ritesh <ritesh.sonar111@gmail.com>
2023-11-13 14:54:42 +0530

    gradle changes for jet compose

---
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+            Text(text = "Hello Ritesh")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, name = "Hello World", showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ComposeDemoTheme {
+        Greeting("Android")
+    }
+}
