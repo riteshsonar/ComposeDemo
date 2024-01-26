b9b1b814e9b618b1f0a6ec21550fccafedccf3dd
Ritesh <ritesh.sonar111@gmail.com>
2024-01-26 19:43:38 +0530

    Create data class

---
+++ app/src/main/java/com/example/composedemo/Data/Language.kt
@@ -0,0 +1,3 @@
+package com.example.composedemo.Data
+
+data class Language(val name:String, val code:String)
---
+++ app/src/main/java/com/example/composedemo/Data/Model/Country.kt
@@ -0,0 +1,3 @@
+package com.example.composedemo.Data.Model
+
+data class Country(val name:String,val code:String)
