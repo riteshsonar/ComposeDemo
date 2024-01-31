81d5e657cb29a1d0f4db740cd3f8891d49fa80fd
Ritesh <ritesh.sonar111@gmail.com>
2024-01-31 16:04:06 +0530

    add some pattern

--- app/src/main/java/com/example/composedemo/sample.kt
+++ app/src/main/java/com/example/composedemo/sample.kt
@@ -4,11 +4,21 @@ fun String.removeFirstLastChar():String = this.substring(1,this.length-1)
 
 fun main(args:Array<String>){
     val myString="Hello Everyone"
-    val result=myString.removeFirstLastChar()
 
+    solvePattern(5)
 
 }
-fun App(){
+private fun solvePattern( N:Int){
+    for (i in 0 until N) {
+        // This is the inner loop which here, loops for the columns
+        // as we have to print a rectangular pattern.
+        for (j in 0 until i) {
+            print("* ")
+        }
 
-
+        // As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        println()
+    }
 }
