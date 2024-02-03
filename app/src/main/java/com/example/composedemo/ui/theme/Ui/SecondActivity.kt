7d8b82ac7c2c4592e6c74f7f062328d3cb23229c
Ritesh <ritesh.sonar111@gmail.com>
2024-02-03 17:31:33 +0530

    add text in activity

--- app/src/main/java/com/example/composedemo/ui/theme/Ui/SecondActivity.kt
+++ app/src/main/java/com/example/composedemo/ui/theme/Ui/SecondActivity.kt
@@ -2,11 +2,28 @@ package com.example.composedemo.ui.theme.Ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.composedemo.R
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContent {
+            PreviewFunction()
+        }
+       // setContentView(R.layout.activity_second)
     }
+}
+
+@Preview(showBackground = true, widthDp = 400, heightDp = 400)
+@Composable
+fun PreviewFunction(){
+    //MessageCard("Ritesh")
+        Text(text = "Hello")
+
 }
