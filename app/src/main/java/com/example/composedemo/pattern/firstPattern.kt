db2b6ba033e96c3ea96c1af5aff325dc14acf8f7
Ritesh <ritesh.sonar111@gmail.com>
2024-02-04 17:40:05 +0530

    add text in activity

---
+++ app/src/main/java/com/example/composedemo/pattern/firstPattern.kt
@@ -0,0 +1,20 @@
+package com.example.composedemo.pattern
+
+fun main(){
+    solvePattern(10)
+}
+
+private fun solvePattern( N:Int){
+    for (i in 0 until N) {
+        // This is the inner loop which here, loops for the columns
+        // as we have to print a rectangular pattern.
+        for (j in 0 until N) {
+            print("$i ")
+        }
+
+        // As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        println()
+    }
+}
