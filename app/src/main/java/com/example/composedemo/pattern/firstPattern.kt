5f4901e7ae4e76747a8077c0304d79cedfebc08f
Ritesh <ritesh.sonar111@gmail.com>
2024-02-07 23:23:19 +0530

    pattern added

--- app/src/main/java/com/example/composedemo/pattern/firstPattern.kt
+++ app/src/main/java/com/example/composedemo/pattern/firstPattern.kt
@@ -1,7 +1,8 @@
 package com.example.composedemo.pattern
 
 fun main(){
-    solvePattern(10)
+    //solvePattern(10)
+    anotherPattern(10)
 }
 
 private fun solvePattern( N:Int){
@@ -15,6 +16,17 @@ private fun solvePattern( N:Int){
         // As soon as N stars are printed, we move to the
         // next row and give a line break otherwise all stars
         // would get printed in 1 line.
+        println()
+    }
+}
+private fun anotherPattern(n:Int){
+    for (i in 1 until n){
+        for(j in 1 until n){
+            for(k in 1 until n/(i+j)){
+                print("* ")
+
+            }
+        }
         println()
     }
 }
