d9e15844226ebb2d255b708d5cd27c8b168198c4
Ritesh <ritesh.sonar111@gmail.com>
2024-02-10 00:26:53 +0530

    extention fun use

--- app/src/main/java/com/example/composedemo/extentionFuncall.kt
+++ app/src/main/java/com/example/composedemo/extentionFuncall.kt
@@ -2,7 +2,7 @@ package com.example.composedemo
 
 //Create an extension for ArrayList which accepts only Int type to return all the even numbers using collections
 fun main() {
-    val arr1 = arrayListOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+    val arr1 = arrayListOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,12)
     val kotlin = arr1.getEvenNumber()
     println(kotlin)
 }
