5490260da0805b4b413c5ab0e65259b6b0952fdb
Ritesh <ritesh.sonar111@gmail.com>
2024-02-20 18:01:34 +0530

    Add Image on compose

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -5,12 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -23,8 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -48,13 +53,25 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
 fun PreviewFunction(){
-    Box(contentAlignment = Alignment.Center){
-        Image(painter = painterResource(id = R.drawable.baseline_heart_broken_24), contentDescription ="abc" )
-        Image(painter = painterResource(id = R.drawable.ic_arrow_right_alt_24), contentDescription ="abc" )
+    Column {
+        ListViewItem(R.drawable.baseline_heart_broken_24,"Ritesh Bagul","Software Devloper")
+        ListViewItem(R.drawable.baseline_heart_broken_24,"Ritesh Bagul","Software Devloper")
+        ListViewItem(R.drawable.baseline_heart_broken_24,"Ritesh Bagul","Software Devloper")
+        ListViewItem(R.drawable.baseline_heart_broken_24,"Ritesh Bagul","Software Devloper")
+        ListViewItem(R.drawable.baseline_heart_broken_24,"Ritesh Bagul","Software Devloper")
+
     }
-
-
-
+}
+@Composable
+fun ListViewItem(imgId: Int, name: String,occupation:String){
+    Row(Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = imgId),
+            contentDescription = "abc",Modifier.size(40.dp))
+        Column (verticalArrangement = Arrangement.SpaceEvenly){
+            Text(text = name, fontWeight = FontWeight.Bold)
+            Text(text = occupation, fontWeight = FontWeight.Light, fontSize = 14.sp)
+        }
+    }
 }
 
 
